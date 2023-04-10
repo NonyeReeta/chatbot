a6925ccf83bb97e20e20b62d5dcff0bb4e9389c2
nonyereeta <nonyeibeanu@gmail.com>
2023-04-10 23:03:46 +0100

    PORT error fixed

--- public/index.js
+++ public/index.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     if (inputValue === "1") {
       // load food menu
       axios
-        .get("https://127.0.0.1:8080/chat/get-menu")
+        .get("http://127.0.0.1:8080/chat/get-menu")
         .then((res) => {
           res.data.map((food) => {
             $("#chat").append(
